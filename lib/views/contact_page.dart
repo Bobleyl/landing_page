eadf2a2005792f9e46fd9fae9ffd8400668967ff
Bo <bleyldev@gmail.com>
2020-10-11 11:57:27 -0400

    contact page done

--- lib/views/contact_page.dart
+++ lib/views/contact_page.dart
@@ -1,7 +1,11 @@
 import 'package:bleyldev_website/views/widgets/social_info.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import '../util/responsive_widget.dart';
+// ignore: avoid_web_libraries_in_flutter
+import 'dart:html' as html;
+import '../util/my_flutter_app_icons.dart' as CustomIcons;
 
 class ContactPage extends StatefulWidget {
   ContactPage({Key key}) : super(key: key);
@@ -241,9 +245,9 @@ class _ContactPageState extends State<ContactPage> {
         physics: ClampingScrollPhysics(),
         child: Column(
           children: [
-            AboutInfo(),
+            ContactInfo(),
             SizedBox(
-              height: MediaQuery.of(context).size.height * 0.2,
+              height: MediaQuery.of(context).size.height * 0.15,
             ),
             SocialInfo(),
           ],
@@ -253,8 +257,8 @@ class _ContactPageState extends State<ContactPage> {
   }
 }
 
-class AboutInfo extends StatelessWidget {
-  const AboutInfo({Key key}) : super(key: key);
+class ContactInfo extends StatelessWidget {
+  const ContactInfo({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -262,99 +266,224 @@ class AboutInfo extends StatelessWidget {
     final profileData = Column(
       mainAxisAlignment: MainAxisAlignment.start,
       children: [
-        SizedBox(height: screenSize.height / 10),
+        SizedBox(height: screenSize.height / 8),
         Row(
           children: [
-            Text(
-              "Hi, I'm ",
-              style: TextStyle(
-                fontSize: 32,
-                color: Colors.white,
+            Container(
+              decoration: BoxDecoration(
+                color: Color(0xff8c53ff),
+                borderRadius: BorderRadius.circular(15.0),
+              ),
+              height: screenSize.height / 3,
+              width: 300,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    "Flutter Help",
+                    style: GoogleFonts.lato(
+                      fontSize: 20,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "FREE",
+                        style: GoogleFonts.lato(
+                          fontSize: 60,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Text(
+                        "Have a question you",
+                        style: GoogleFonts.lato(
+                          fontSize: 14,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Text(
+                        "need help with?",
+                        style: GoogleFonts.lato(
+                          fontSize: 14,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    width: 250,
+                    height: 65,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                    child: MaterialButton(
+                      onPressed: () {
+                        html.window.open(
+                          "mailto:bleyldev@gmail.com",
+                          "Email",
+                        );
+                      },
+                      child: Center(
+                        child: Text(
+                          "Contact Now",
+                          style: GoogleFonts.lato(
+                            fontSize: 24.0,
+                            color: Color(0xff8c53ff),
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-            Text(
-              "Bo!",
-              style: TextStyle(
-                fontSize: 32,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
+            SizedBox(width: 15.0),
+            Container(
+              decoration: BoxDecoration(
+                color: Color(0xff8c53ff),
+                borderRadius: BorderRadius.circular(15.0),
+              ),
+              height: screenSize.height / 3,
+              width: 300,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    "Flutter Consulting",
+                    style: GoogleFonts.lato(
+                      fontSize: 20,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "\$35",
+                        style: GoogleFonts.lato(
+                          fontSize: 60,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Text(
+                        "  / Hour",
+                        style: GoogleFonts.lato(
+                          fontSize: 20,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Text(
+                        " - Dedicated Project Help",
+                        style: GoogleFonts.lato(
+                          fontSize: 14,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Text(
+                        " - Help Building Features",
+                        style: GoogleFonts.lato(
+                          fontSize: 14,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    width: 250,
+                    height: 65,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                    child: MaterialButton(
+                      onPressed: () {
+                        html.window.open(
+                          "mailto:bleyldev@gmail.com",
+                          "Email",
+                        );
+                      },
+                      child: Center(
+                        child: Text(
+                          "Contact Now",
+                          style: GoogleFonts.lato(
+                            fontSize: 24.0,
+                            color: Color(0xff8c53ff),
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
         ),
-        SizedBox(height: screenSize.height / 50),
-        Container(
-          width: 250.0,
-          height: 250.0,
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            image: DecorationImage(
-              fit: BoxFit.fill,
-              image: NetworkImage('/assets/profile.jpg'),
-            ),
-          ),
-        ),
-        SizedBox(height: screenSize.height / 50),
+        SizedBox(height: screenSize.height / 10),
         Text(
-          """          About Me: My name is Bo, I’m a Software Engineer and a Teacher of 
-          Flutter.  I manage a youtube channel called Bleyl Dev that teaches 
-          Developers how to better their Flutter skills.  I’ve spent the last 8 
-          years coding as well as teaching kids and adults alike how to code.
-      
-          My long-term hope is that Bleyl Dev can grow to provide
-          free Flutter help to all!  In addition to my current Youtube videos I'm 
-          working on, I'm currently working on \"Flutter Jr\", a Flutter coding 
-          course that teaches kids how to code with Flutter and Dart.  If you want 
-          to get to know me more or need help with a Flutter project, please be 
-          sure to join our discord!  Connect with other developers and get quick 
-          Flutter help when you need it.  Link is in footer at bottom of webpage 😉
-          """,
-          style: TextStyle(
-            fontSize: 20,
+          "Other ways to get in touch",
+          style: GoogleFonts.lato(
+            fontSize: 36,
             color: Colors.white,
           ),
         ),
-        SizedBox(height: screenSize.height / 10),
+        SizedBox(height: screenSize.height / 20),
         Row(
           children: [
-            Text(
-              "About ",
-              style: TextStyle(
-                fontSize: 32,
+            IconButton(
+              iconSize: 100,
+              icon: Icon(
+                CustomIcons.MyFlutterApp.mail_squared,
                 color: Colors.white,
               ),
+              onPressed: () {
+                html.window.open(
+                  "mailto:bleyldev@gmail.com",
+                  "Email",
+                );
+              },
             ),
-            Text(
-              "Bleyl Dev",
-              style: TextStyle(
-                fontSize: 32,
-                fontWeight: FontWeight.bold,
+            IconButton(
+              iconSize: 100,
+              icon: Icon(
+                CustomIcons.MyFlutterApp.twitter_squared,
                 color: Colors.white,
               ),
+              onPressed: () {
+                html.window.open(
+                  "https://twitter.com/bleyldev",
+                  "Twitter",
+                );
+              },
+            ),
+            IconButton(
+              iconSize: 95,
+              icon: Icon(
+                CustomIcons.MyFlutterApp.discord,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                html.window.open(
+                  "https://discord.gg/aUQj87U",
+                  "Discord",
+                );
+              },
             ),
           ],
-        ),
-        SizedBox(height: screenSize.height / 50),
-        Text(
-          """        Bleyl Dev is a channel devoted to teaching Flutter.  Our goal is to 
-        provide weekly, quality flutter content.  If you aren't already subscribed
-        to the channel, be sure to do so now!  If you have any topics that you 
-        are struggling with and would like to see a video on it on our channel,
-        please reach out via our \'Contact\' page and let us know!
-        
-        Interested in being a part of the Bleyl Dev team? If you enjoy
-        teaching others and have a knack for video editing, we'd love to look at
-        your content and potentially post a video of yours on the channel.  Just 
-        reach out and let us know you're interested.  
-
-        Want to contribute and help others?  Join our discord or reach out to 
-        potentially be a moderator and help out with handling Flutter and Dart
-        related questions from the community.
-        """,
-          style: TextStyle(
-            fontSize: 20,
-            color: Colors.white,
-          ),
         ),
       ],
     );
@@ -363,12 +492,16 @@ class AboutInfo extends StatelessWidget {
       largeScreen: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[profileData],
+        children: <Widget>[
+          profileData,
+        ],
       ),
       smallScreen: Column(
         mainAxisSize: MainAxisSize.max,
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[profileData],
+        children: <Widget>[
+          profileData,
+        ],
       ),
     );
   }
